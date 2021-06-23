5df5553d94689b4a13b389a091f03f7c9a103ea9
AmirBolouri <amirbolouri22@gmail.com>
2021-06-24 03:24:31 +0430

    - Enabled fall damage - Player now gets one point every second

--- Main/main_loop.py
+++ Main/main_loop.py
@@ -21,8 +21,14 @@ while run:
             scrollBackground(scrollSpeed, 0)  # harekate background
             spawnerE.spawnHazard(320, 96,"images/spr_enemy.png",enemylist,type= "Enemy")
             spawnerS.spawnHazard(346, 69, "images/spr_spike.png", spikelist,type= "Spike")
-        # if pl.ypos > 450 or pl.ypos < -32:
-        #     pl.despawn()
+
+
+        if pl.ypos > 450 or pl.ypos < -32: #fall damage
+            pl.despawn()
+
+        if clock() > targetTime:
+            targetTime = clock() + 1000 #points per second
+            scoreboard.Addscore(1)
 
         # if (keyPressed("left")): #in size windows ro tagheer mide. shayad be dard bokhore baadan.
         #     W , H = H , W
@@ -40,9 +46,7 @@ while run:
         #     bgs = 0
 
 
-        # if clock() > targetTime:
-        #     targetTime = clock() + 2000
-        #     scoreboard.Addscore(1)
+
 
         if (keyPressed("k")) and pl.alive == False: #kelid k = koshtan player
             pl.despawn()
